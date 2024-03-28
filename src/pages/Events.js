23d95cc2e1b37616905fd86f4b7e460d57f2ffce
Kaja <kaja.piechowka@gmail.com>
2024-03-28 11:30:02 +0100

    hide components that block the app

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,149 +1,149 @@
-import React from "react"
-import Navigation from "../components/Navigation/Navigation"
-import Footer from "../components/Footer/Footer"
-import styled from "styled-components"
-import { colors, fonts } from "../style/variables"
-import { graphql, useStaticQuery } from "gatsby"
-import Image from "gatsby-image"
-import SectionHeader from "../components/SectionHeader/SectionHeader"
-import EventsWrapper from "../components/Events/EventsImage"
+// import React from "react"
+// import Navigation from "../components/Navigation/Navigation"
+// import Footer from "../components/Footer/Footer"
+// import styled from "styled-components"
+// import { colors, fonts } from "../style/variables"
+// import { graphql, useStaticQuery } from "gatsby"
+// import Image from "gatsby-image"
+// import SectionHeader from "../components/SectionHeader/SectionHeader"
+// import EventsWrapper from "../components/Events/EventsImage"
 
-const OneEventWrapper = styled.div`
-  width: 100%;
-  height: 270px;
-  display: flex;
-  margin-top: 100px;
-`
+// const OneEventWrapper = styled.div`
+//   width: 100%;
+//   height: 270px;
+//   display: flex;
+//   margin-top: 100px;
+// `
 
-const OneEvent = styled.div`
-  width: 100vw;
-  position: relative;
-  z-index: 5;
-  p {
-    color: ${colors.text};
-  }
-`
-const EventImage = styled.div`
-  width: 100%;
-  height: auto;
-  position: absolute;
-`
+// const OneEvent = styled.div`
+//   width: 100vw;
+//   position: relative;
+//   z-index: 5;
+//   p {
+//     color: ${colors.text};
+//   }
+// `
+// const EventImage = styled.div`
+//   width: 100%;
+//   height: auto;
+//   position: absolute;
+// `
 
-const EventTextWrapperHolder = styled.div`
-  width: 40%;
-  height: 30%;
-  background-color: ${colors.window};
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  bottom: 0;
-`
-const EventTextWrapper = styled.button`
-  width: 85%;
-  height: 85%;
-  background-color: ${colors.background};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  border: none;
-  &:hover {
-    outline: none;
-    transform: scale(1.1);
-  }
-`
-const EventTitle = styled.span`
-  font-family: ${fonts.main};
-  color: ${colors.text};
-  font-size: 1.1rem;
-`
-const EventDate = styled.span`
-  font-family: ${fonts.main};
-  color: ${colors.text};
-  font-size: 1rem;
-`
+// const EventTextWrapperHolder = styled.div`
+//   width: 40%;
+//   height: 30%;
+//   background-color: ${colors.window};
+//   position: absolute;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   bottom: 0;
+// `
+// const EventTextWrapper = styled.button`
+//   width: 85%;
+//   height: 85%;
+//   background-color: ${colors.background};
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: column;
+//   border: none;
+//   &:hover {
+//     outline: none;
+//     transform: scale(1.1);
+//   }
+// `
+// const EventTitle = styled.span`
+//   font-family: ${fonts.main};
+//   color: ${colors.text};
+//   font-size: 1.1rem;
+// `
+// const EventDate = styled.span`
+//   font-family: ${fonts.main};
+//   color: ${colors.text};
+//   font-size: 1rem;
+// `
 
-const HeaderWrapper = styled.div`
-  position: absolute;
-  left: 2rem;
-  top: 10rem;
-`
+// const HeaderWrapper = styled.div`
+//   position: absolute;
+//   left: 2rem;
+//   top: 10rem;
+// `
 
-const query = graphql`
-  {
-    allDatoCmsEvent {
-      nodes {
-        id
-        dateTime
-        titlePl
-        imageMain {
-          fluid(maxWidth: 600) {
-            ...GatsbyDatoCmsFluid
-          }
-        }
-      }
-    }
-  }
-`
+// const query = graphql`
+//   {
+//     allDatoCmsEvent {
+//       nodes {
+//         id
+//         dateTime
+//         titlePl
+//         imageMain {
+//           fluid(maxWidth: 600) {
+//             ...GatsbyDatoCmsFluid
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
 
-const EventsPage = () => {
-  const eventsQuery = useStaticQuery(query)
-  return (
-    <>
-      <Navigation />
-      <HeaderWrapper>
-        <SectionHeader>
-          <h1>Wydarzenia</h1>
-        </SectionHeader>
-      </HeaderWrapper>
+// const EventsPage = () => {
+//   const eventsQuery = useStaticQuery(query)
+//   return (
+//     <>
+//       <Navigation />
+//       <HeaderWrapper>
+//         <SectionHeader>
+//           <h1>Wydarzenia</h1>
+//         </SectionHeader>
+//       </HeaderWrapper>
 
-      <EventsWrapper>
-        {eventsQuery.allDatoCmsEvent.nodes.map((event, index) => {
-          if (index % 2 === 0) {
-            return (
-              <OneEventWrapper key={event.id}>
-                <OneEvent>
-                  <EventImage>
-                    <Image fluid={event.imageMain.fluid} />
-                  </EventImage>
-                  <EventTextWrapperHolder style={{ right: 0 }}>
-                    <EventTextWrapper>
-                      <EventTitle>{event.titlePl}</EventTitle>
-                      <EventDate>{event.dateTime.slice(0, 10)}</EventDate>
-                    </EventTextWrapper>
-                  </EventTextWrapperHolder>
-                </OneEvent>
-              </OneEventWrapper>
-            )
-          } else if (index % 2 !== 0) {
-            return (
-              <OneEventWrapper
-                key={event.id}
-                style={{ justifyContent: "flex-end" }}
-              >
-                <OneEvent>
-                  <EventImage style={{ right: 0 }}>
-                    <Image fluid={event.imageMain.fluid} />
-                  </EventImage>
-                  <EventTextWrapperHolder style={{ left: 0 }}>
-                    <EventTextWrapper>
-                      <EventTitle>{event.titlePl}</EventTitle>
-                      <EventDate>{event.dateTime.slice(0, 10)}</EventDate>
-                    </EventTextWrapper>
-                  </EventTextWrapperHolder>
-                </OneEvent>
-              </OneEventWrapper>
-            )
-          } else {
-            return null
-          }
-        })}
-      </EventsWrapper>
+//       <EventsWrapper>
+//         {eventsQuery.allDatoCmsEvent.nodes.map((event, index) => {
+//           if (index % 2 === 0) {
+//             return (
+//               <OneEventWrapper key={event.id}>
+//                 <OneEvent>
+//                   <EventImage>
+//                     <Image fluid={event.imageMain.fluid} />
+//                   </EventImage>
+//                   <EventTextWrapperHolder style={{ right: 0 }}>
+//                     <EventTextWrapper>
+//                       <EventTitle>{event.titlePl}</EventTitle>
+//                       <EventDate>{event.dateTime.slice(0, 10)}</EventDate>
+//                     </EventTextWrapper>
+//                   </EventTextWrapperHolder>
+//                 </OneEvent>
+//               </OneEventWrapper>
+//             )
+//           } else if (index % 2 !== 0) {
+//             return (
+//               <OneEventWrapper
+//                 key={event.id}
+//                 style={{ justifyContent: "flex-end" }}
+//               >
+//                 <OneEvent>
+//                   <EventImage style={{ right: 0 }}>
+//                     <Image fluid={event.imageMain.fluid} />
+//                   </EventImage>
+//                   <EventTextWrapperHolder style={{ left: 0 }}>
+//                     <EventTextWrapper>
+//                       <EventTitle>{event.titlePl}</EventTitle>
+//                       <EventDate>{event.dateTime.slice(0, 10)}</EventDate>
+//                     </EventTextWrapper>
+//                   </EventTextWrapperHolder>
+//                 </OneEvent>
+//               </OneEventWrapper>
+//             )
+//           } else {
+//             return null
+//           }
+//         })}
+//       </EventsWrapper>
 
-      <Footer />
-    </>
-  )
-}
+//       <Footer />
+//     </>
+//   )
+// }
 
-export default EventsPage
+// export default EventsPage
